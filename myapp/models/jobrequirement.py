167d5c43e6bb56141a1773990454c388d8c9085d
Ubuntu <ubuntu@intern-test.intern-test>
2025-06-09 11:09:58 +0000

    DB changes

--- myapp/models/jobrequirement.py
+++ myapp/models/jobrequirement.py
@@ -3,7 +3,7 @@ from myapp.extensions import db
 class JobRequirement(db.Model):
     __tablename__ = 'jobrequirement'
 
-    job_id = db.Column(db.Integer, primary_key=True)
-    job_position = db.Column(db.String(50), nullable = False)
-    job_desc = db.Column(db.Text, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    position = db.Column(db.String(50), nullable = False)
+    desc = db.Column(db.Text, nullable = False)
     
