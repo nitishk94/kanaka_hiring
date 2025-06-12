3d89eb338588af39672fb633a046e6b01e542fef
Ubuntu <ubuntu@intern-test.intern-test>
2025-06-12 06:40:33 +0000

    using .env

--- myapp/config.py
+++ myapp/config.py
@@ -1,9 +1,15 @@
-import secrets
+from dotenv import load_dotenv
 from datetime import timedelta
+from pathlib import Path
+import secrets
+import os
+
+env_path = Path(__file__).resolve().parents[1] / '.env'
+load_dotenv(dotenv_path=env_path)
 
 class Config:
     SECRET_KEY = secrets.token_hex(32)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:watts25@192.168.1.205:5432/kanaka_hiring'
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
     SESSION_COOKIE_SECURE = False
